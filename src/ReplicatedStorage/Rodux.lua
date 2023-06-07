7ddd5ceb72f8b5745bc5c9c348910b1f58ec8ff4
lisachandra <>
2023-06-07 12:16:42 +0700

    add useState hook

--- src/ReplicatedStorage/Rodux.lua
+++ src/ReplicatedStorage/Rodux.lua
@@ -2,12 +2,18 @@ local RunService = game:GetService("RunService")
 
 local Packages = script.Parent.Parent.Packages
 
+local RoactHooks = require(Packages.RoactHooks)
 local Rodux = require(Packages.Rodux)
+local Sift = require(Packages.Sift)
+
+local updaters: Dictionary<(newValue: any) -> ()> = {}
 
 local initialState: ClientState | ServerState
 local creators: Dictionary<creator>
 local reducers: Dictionary<reducer>
 local middlewares: Array<middleware>
+
+local store: Store
 
 type middleware = (nextDispatch: (action: action) -> (), store: Store) -> ((action: action) -> ())
 type creator = (...any) -> action
@@ -19,12 +25,21 @@ export type ServerState = {}
 
 export type Store = typeof(Rodux.Store.new(nil :: any, nil :: any, nil :: any, nil :: any))
 
-local function reducer(state, action: action)
+local function reducer(state: Dictionary<any>, action: action)
     local newState = {}; for key, reducer in reducers do
         newState[key] = reducer(state[key], action)
     end
 
     return newState
+end
+
+local function useState(hooks: RoactHooks.Hooks, key: string)
+    local state = store:getState()
+    local value, update = hooks.useState(state[key])
+
+    updaters[key] = update
+
+    return value
 end
 
 if RunService:IsClient() then
@@ -39,16 +54,30 @@ else
     middlewares = { Rodux.thunkMiddleware :: any }
 end
 
+store = Rodux.Store.new(reducer, initialState, middlewares, {
+    reportReducerError = function(prevState, action, errorResult)
+        error(("Received error: %s\n\n%s"):format(errorResult.message, errorResult.thrownValue))
+    end,
+
+    reportUpdateError = function(prevState, currentState, lastActions, errorResult)
+        error(("Received error: %s\n\n%s"):format(errorResult.message, errorResult.thrownValue))
+    end,
+})
+
+store.changed.connect(store :: any, function(new, old)
+    for key, update in updaters do
+        if type(new[key]) == "table" and type(old[key]) == "table" then
+            if not Sift.Dictionary.equals(new[key], old[key]) then
+                update(new[key])
+            end
+        else
+            update(new[key])
+        end
+    end
+end)
+
 return {
-    store = Rodux.Store.new(reducer, initialState :: table, middlewares :: table, {
-        reportReducerError = function(prevState, action, errorResult)
-            error(string.format("Received error: %s\n\n%s", errorResult.message, errorResult.thrownValue))
-        end,
-
-        reportUpdateError = function(prevState, currentState, lastActions, errorResult)
-            error(string.format("Received error: %s\n\n%s", errorResult.message, errorResult.thrownValue))
-        end,
-    }),
-
+    store = store,
+    useState = useState,
     creators = creators,
 }
--- src/ServerScriptService/Hooks/useDependency.lua
+++ src/ServerScriptService/Hooks/useDependency.lua
@@ -10,13 +10,13 @@ type storage = {
 	dependencies: Array<any>?,
 }
 
-local function useDependency(callback: (last: Array<any>?, new: Array<any>?) -> (), dependencies: Array<any>?, discriminator: string)
+local function useDependency(callback: (old: Array<any>?, new: Array<any>?) -> (), dependencies: Array<any>?, discriminator: string)
 	local storage: table = Matter.useHookState(discriminator, function()
 		return true
 	end)
 
 	if
-		((storage.dependencies and dependencies) and Sift.Array.equals(dependencies, storage.dependencies))
+		((storage.dependencies and dependencies) and not Sift.Array.equals(dependencies, storage.dependencies))
 		or (storage.dependencies == nil or dependencies == nil)
 	then
 		callback(storage.dependencies, dependencies)
