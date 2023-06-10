7da2c52a75a7b92c8cd96ca727b30d261b94b151
lisachandra <>
2023-06-10 17:44:50 +0700

    add local component replication

--- src/ServerScriptService/Systems/replication.lua
+++ src/ServerScriptService/Systems/replication.lua
@@ -3,8 +3,10 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Shared = ReplicatedStorage.Shared
 local Remotes = ReplicatedStorage.Remotes
+local Packages = ReplicatedStorage.Packages
 
 local Matter = require(Shared.Matter)
+local Sift = require(Packages.Sift)
 
 local Components = require(Shared.Components)
 
@@ -14,15 +16,26 @@ local REPLICATED_COMPONENTS = {
 	"PlayerData",
 }
 
-local replicatedComponents: { [Matter.Component<any>]: boolean } = {}
+local LOCAL_REPLICATED_COMPONENTS = {}
 
-for _index, name: string in REPLICATED_COMPONENTS do
+local replicatedComponents: { [Matter.Component<any>]: boolean } = {}
+local localReplicatedComponents: { [Matter.Component<any>]: boolean } = {}
+
+for _index, name: string in ipairs(REPLICATED_COMPONENTS) do
 	replicatedComponents[Components[name]] = true
 end
 
+for _index, name: string in ipairs(LOCAL_REPLICATED_COMPONENTS) do
+	localReplicatedComponents[Components[name]] = true
+end
+
+local mergedReplicatedComponents = Sift.Dictionary.merge(replicatedComponents, localReplicatedComponents) 
+
+type payload = Dictionary<Dictionary<{ data: table }>>
+
 local function replication(world: Matter.World)
 	for _index, player: Player in Matter.useEvent(Players, "PlayerAdded") do
-		local payload = {}
+		local payload: payload = {}
 
 		for entityId, entityData in world do
 			local entityPayload = {}
@@ -39,25 +52,42 @@ local function replication(world: Matter.World)
 		MatterRemote:FireClient(player, payload)
 	end
 
-	local changes = {}
+	local payloads: Dictionary<payload> = {}
 
-	for component in replicatedComponents do
+	for component in mergedReplicatedComponents do
+		local isLocalComponent = table.find(component, localReplicatedComponents) and true or false
+
 		for entityId, record in world:queryChanged(component) do
-			local key = `{entityId}`
-			local name = `{component}`
+			for _index, PlayerData in world:query(Components.PlayerData) do
+				local playerEntityId = PlayerData.Player:GetAttribute("serverEntityId")
 
-			if changes[key] == nil then
-				changes[key] = {}
-			end
+				if isLocalComponent and entityId ~= playerEntityId then continue end
 
-			if world:contains(entityId) then
-				changes[key][name] = { data = record.new }
+				local userId = `{PlayerData.Player.UserId}`
+				local payload = payloads[userId] or {} :: payload
+
+				local key = `{entityId}`
+				local name = `{component}`
+
+				if payload[key] == nil then
+					payload[key] = {}
+				end
+
+				if world:contains(entityId) then
+					payload[key][name] = { data = record.new :: any }
+				end
+
+				payloads[userId] = payload
 			end
 		end
 	end
 
-	if next(changes) then
-		MatterRemote:FireAllClients(changes)
+	if next(payloads) then
+		for userId, payload in payloads do
+			local player = Players:GetPlayerByUserId(tonumber(userId))
+
+			MatterRemote:FireClient(player, payload)
+		end
 	end
 end
 
